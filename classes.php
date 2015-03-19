0875704227ebe3361a03d83b91e9b0d01ab189d9
J3K <jamal.eddine.kaouiri@gmail.com>
2015-03-19 09:28:27 +0000

    Wednesday

---
+++ classes.php
@@ -0,0 +1,10 @@
+<?php
+
+class xmlMatches{
+
+
+$url = "http://api.livesportstreams.watch/v1/feed/550684e3b6b6e818801925/10/10/720/480";
+
+}
+
+?>
--- index.php
+++ index.php
@@ -83,7 +83,18 @@ if (file_exists($cachefile) && (time() - $cachetime < filemtime($cachefile))) {
 
 <div class="container-fluid">
     <div class="row">
+
         <div class="col-md-9">
+             <div class="panel panel-default">
+                <div class="panel-heading"> 
+                    <span class="text-center">
+                        <strong>
+                            <h5><font color="darkgreen">Stable Streaming Source</font>
+                        </strong>
+                    </span>
+                </div>
+            </div>
+
             <?php
             include('simple_html_dom.php');
 
@@ -429,6 +440,17 @@ $(document).ready(function() {
 
 <?php } ?>
 
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-52778373-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 <?php
     $fp = fopen($cachefile, 'w'); 
